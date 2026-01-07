acbdcadaeef60febe7293efe59f0f2551732cba3
[dyad] <joaquimcruz3110@gmail.com>
2026-01-07 12:39:55 -0300

    [dyad] Removendo importmap e criando index.css para resolver erros de runtime - wrote 3 file(s)

--- index.tsx
+++ index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
 import { SessionContextProvider } from './src/contexts/SessionContextProvider.tsx';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
