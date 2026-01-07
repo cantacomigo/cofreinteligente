a5bb6c28694714fc66468e9bbeadfaede6828943
[dyad] <joaquimcruz3110@gmail.com>
2026-01-07 11:57:43 -0300

    [dyad] Corrigindo erros de resolução de módulo do TypeScript - wrote 2 file(s)

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { SessionContextProvider } from './contexts/SessionContextProvider';
+import { SessionContextProvider } from './contexts/SessionContextProvider.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
